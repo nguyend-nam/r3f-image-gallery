d23fe18a2bdcf634aef423eb58020f1b4fa77f1d
nguyend-nam <nguyennamnade22@gmail.com>
2023-01-28 22:01:02 +0700

    chore: update arts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
     <Html>
       <Head>
         <title>R3f Image Gallery</title>
+        <meta name="theme-color" content="#00e0c4" />
+        <meta property="og:image" content="/og.png" />
+        <meta property="og:url" content="https://dwarvesf-r3f.netlify.app/" />
+        <meta property="og:title" content="R3f Image Gallery" />
         <meta
           name="description"
           content="React three fiber image grid layout"
