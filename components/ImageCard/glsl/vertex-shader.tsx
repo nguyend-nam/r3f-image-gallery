e684a64a634590b8d931d09b2434fdec76b7fc5e
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2024-08-12 15:43:52 +0700

    chore: show hover effect

--- components/ImageCard/glsl/vertex-shader.tsx
+++ components/ImageCard/glsl/vertex-shader.tsx
@@ -12,15 +12,15 @@ export const vertexShader = `
     vec2 noiseFreq = vec2(2.0, 1.5); // frequency of wave
     vec2 noiseAmp = vec2(0.1, 0.07); // amplitude of wave
 
-    // if(uniformHover == 1.0) {
-    //   pos.z += sin(pos.x * noiseFreq.x - uniformMousePosition.x) * noiseAmp.x;
-    //   pos.z += sin(pos.y * noiseFreq.y - uniformMousePosition.y) * noiseAmp.y;
-    // }
+    if(uniformHover == 1.0) {
+      pos.z += sin(pos.x * noiseFreq.x - uniformMousePosition.x) * noiseAmp.x;
+      pos.z += sin(pos.y * noiseFreq.y - uniformMousePosition.y) * noiseAmp.y;
+    }
     
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
+    // gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 
-    // vec4 modelPosition = modelMatrix * vec4(pos, 1.0);
-    // modelPosition.z += sin(modelPosition.x * noiseFreq + uniformTime) * noiseAmp;
-    // gl_Position = projectionMatrix * viewMatrix * modelPosition;
+    vec4 modelPosition = modelMatrix * vec4(pos, 1.0);
+    modelPosition.z += sin(modelPosition.x * noiseFreq + uniformTime) * noiseAmp;
+    gl_Position = projectionMatrix * viewMatrix * modelPosition;
   }
 `
